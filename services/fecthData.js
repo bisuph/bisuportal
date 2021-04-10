a4faa2d9af633bd0a76b6d12e9e3a263942e4652
donsscalabss <donzcalabz@gmail.com>
2021-04-10 22:30:43 +0800

    update dir

--- services/fecthData.js
+++ services/fecthData.js
@@ -1,4 +1,4 @@
-import { auth, db } from "../services/firebase";
+import { auth, db } from "./firebase";
 
 export async function getCampuses() {
     const snapshot = await  db.collection("Campuses").get()
